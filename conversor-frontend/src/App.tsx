2000e981adf39e08b40844f408d0a1b170adc302
Lucas Karr <lucas.karr@roxcode.dev>
2023-11-27 10:14:19 -0300

    bonitim

--- conversor-frontend/src/App.tsx
+++ conversor-frontend/src/App.tsx
@@ -1,10 +1,5 @@
-// routes
 import Router from './routes';
-// theme
 import ThemeProvider from './theme';
-// components
-// import Settings from './components/settings';
-// import RtlLayout from './components/RtlLayout';
 
 // ----------------------------------------------------------------------
 
